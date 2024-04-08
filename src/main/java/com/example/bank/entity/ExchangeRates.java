a200c18ff3f504432a089ef023ce981dd7600f4d
arsen tadevosyan <arsentadevosyan370@gmail.com>
2024-04-08 22:25:00 +0400

    cb Api corrections

--- src/main/java/com/example/bank/entity/ExchangeRates.java
+++ src/main/java/com/example/bank/entity/ExchangeRates.java
@@ -1,7 +1,13 @@
 package com.example.bank.entity;
 
 import com.example.bank.entity.enums.MoneyType;
-import jakarta.persistence.*;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
