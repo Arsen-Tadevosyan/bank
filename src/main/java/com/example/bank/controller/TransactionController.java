19b5d65252ca6b977d56ed675e4d5135d8bfd25e
Narek Baghdasaryan <baxdasaryann2008@gmail.com>
2024-05-27 19:56:56 +0400

    about us 2

--- src/main/java/com/example/bank/controller/TransactionController.java
+++ src/main/java/com/example/bank/controller/TransactionController.java
@@ -2,12 +2,20 @@ package com.example.bank.controller;
 
 
 import com.example.bank.dto.CreditRequestDto;
-import com.example.bank.entity.*;
+import com.example.bank.entity.Card;
+import com.example.bank.entity.Notification;
+import com.example.bank.entity.Repay;
+import com.example.bank.entity.Transaction;
+import com.example.bank.entity.User;
 import com.example.bank.entity.enums.NotificationType;
 import com.example.bank.entity.enums.Status;
 import com.example.bank.entity.enums.TransactionType;
 import com.example.bank.security.CurrentUser;
-import com.example.bank.service.*;
+import com.example.bank.service.CardService;
+import com.example.bank.service.RepayService;
+import com.example.bank.service.NotificationService;
+import com.example.bank.service.TransactionService;
+import com.example.bank.service.TransferService;
 import com.example.bank.service.impl.SendMailService;
 import com.example.bank.util.FileUtil;
 import io.micrometer.common.util.StringUtils;
