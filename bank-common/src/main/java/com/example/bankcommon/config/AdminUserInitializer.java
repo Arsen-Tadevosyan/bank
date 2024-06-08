9f2450eff33862b153e5b54936c10ae5167ef7eb
arsen tadevosyan <arsentadevosyan370@gmail.com>
2024-06-08 21:02:10 +0400

    Corrections

--- bank-common/src/main/java/com/example/bankcommon/config/AdminUserInitializer.java
+++ bank-common/src/main/java/com/example/bankcommon/config/AdminUserInitializer.java
@@ -39,4 +39,5 @@ public class AdminUserInitializer {
             log.info("Admin user already exists.");
         }
     }
+
 }
--- bank-common/src/main/java/com/example/bankcommon/entity/User.java
+++ bank-common/src/main/java/com/example/bankcommon/entity/User.java
@@ -15,7 +15,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
